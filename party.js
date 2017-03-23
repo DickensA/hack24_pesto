3fa2fcc733529991529e0139ecd016f3e51648bf
Bety <bety.mehide@gmail.com>
2017-03-23 15:32:50 +0000

    The pretty version
    Don't know if it works yet.
    - Gathered all the messages to languageStrings; 
    - Renamed the partyList variable to paryItem and the ENDMODE to ACTIONMODE; 
    - Added comments;
    - Moved around if statements in the GetPartyIntent function to save the answer to the previous question before checking for further partyitems;
    - Added repromptSpeech to ask emit if missing before.

--- party.js
+++ party.js
@@ -1,17 +1,14 @@
-//Installed node modules: jquery underscore request express jade shelljs passport http sys lodash async mocha chai sinon sinon-chai moment connect validator restify ejs ws co when helmet wrench brain mustache should backbone forever debug
-
 'use strict';
 
 const Alexa = require('alexa-sdk');
 
-//defining the states
+//setting up states
 var states = {
-    // setting up states
     PARTYMODE: '_PARTYMODE',
-    ENDMODE: '_ENDMODE'
+    ACTIONMODE: '_ACTIONMODE',
 }
 
-//Initializing Alexa and the scroll through the list of events
+//Initializing Alexa
 var  startHandlers ={
     //new session of our skill
     'NewSession': function() {
@@ -20,8 +17,8 @@ var  startHandlers ={
             this.attributes.countResponse = 0;
         }
         this.handler.state = states.PARTYMODE;
-        this.attributes.speechOutput = this.t('We Like to Party is an Alexa Skill that transforms a list of event invitations into a two way conversation. She gently helps you to make decisions, and pushes you back into the real world. With a pinch of dry humor, ahah. WELCOME_MESSAGE');
-        //no reply or a reply that's not recognized will trigger this
+        this.attributes.speechOutput = this.t('WELCOME_MESSAGE');    
+        //no reply or a reply that\'s not recognized will trigger this
         this.attributes.repromptSpeech = this.t('WELCOME_REPROMPT');
         this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
     }
@@ -29,41 +26,38 @@ var  startHandlers ={
 //The function that controls the party listing functionality of Alexa
 var partyHandlers = Alexa.CreateStateHandler(states.PARTYMODE, {
     'NewSession': function() {
-         this.emit('NewSession'); // Uses the handler in startHandlers
+         this.emit('NewSession'); //Uses the handler in startHandlers, doesn't really need to be here but can be adjusted in the future if further flexibility is needed
     },
     //Our own intent function for getting alexa to read out the party list
     'GetPartyIntent' : function() {
-        var partyList = "";
+        var partyItem = "";
+        //this array would ideally be pulled in from an outside source (e.g. using the FB API) in the future. 
         let fakedParties = [
             " Saria's super cool birthday, at The Broadway this Saturday at Midnight ",
             " Amy's awesome rave, in the woods tomorrow at Noon, you'll need a mask and a torch ", 
             " Washing the dishes, with Dad tonight after dinner, love mum ", 
             " An intercity disco, between New York and San Francisco with The Vengaboys, As soon as the Vengabus gets here, I've heard it's coming "
         ];
-        
-            partyList = this.t(fakedParties[this.attributes.eventCounter]);
-            this.attributes.eventCounter++;
-        //check if still have parties left in the list
-        if (partyList) {
-            this.handler.state = states.ENDMODE;
-                if (this.attributes.countResponse == 1) {
-                    this.attributes.speechOutput = this.t('Sure? You should go out more often. Your social life is worse than mine. But okay, this party wasn\'t that cool anyways. Do you want to go to');
-                } else if (this.attributes.countResponse == 2) {
-                    this.attributes.speechOutput = this.t('Cool, I\'ve added it to your calendar. Do you want to go to');
-                } else {
-                    this.attributes.speechOutput = this.t('Do you want to go to');
-                }
+        //find a party from the list
+        partyItem = this.t(fakedParties[this.attributes.eventCounter]);
+        this.attributes.eventCounter++;
+        //Check if need an answer to a previous prompt
+        if (this.attributes.countResponse == 1) {
+                this.attributes.speechOutput = this.t('ANSWER_NO');
+            } else if (this.attributes.countResponse == 2) {
+                this.attributes.speechOutput = this.t('ANSWER_YES');
+            }   
+        //check if still have parties left in the list, if yes, move on to next invite if no give answer and prompt for a new intent. 
+        if (partyItem) {
+            this.handler.state = states.ACTIONMODE;
+            this.attributes.speechOutput += this.t('ASK_FOR_RSVP');
+            this.attributes.repromptSpeech = this.t('ANSWER_REPROMPT');
             //reads out the list
-            this.emit(':ask', this.attributes.speechOutput + partyList, 'Yes or No?');
+            this.emit(':ask', this.attributes.speechOutput + partyItem, this.attributes.repromptSpeech);
         } else {
-                if (this.attributes.countResponse == 1) {
-                    this.attributes.speechOutput = this.t('This party wasn\'t that cool anyways.' + ' NO_PARTIES_IN_LIST');
-                } else if (this.attributes.countResponse == 2) {
-                    this.attributes.speechOutput = this.t('Cool, I\'ve added it to your calendar, whoop whoop.' + ' NO_PARTIES_IN_LIST');
-                } else {
-                    this.attributes.speechOutput = this.t('NO_PARTIES_IN_LIST');
-                }
-            this.emit(':ask', this.attributes.speechOutput);
+            this.attributes.speechOutput += this.t('NO_PARTIES_IN_LIST');
+            this.attributes.repromptSpeech = this.t('WHAT_NEXT');
+            this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
         }
     },
     'AMAZON.HelpIntent': function () {
@@ -85,50 +79,56 @@ var partyHandlers = Alexa.CreateStateHandler(states.PARTYMODE, {
     },
     'Unhandled': function(){
        console.log("UNHANDLED");
-       this.handler.state = states.ENDMODE;
-       var message = 'Say yes to continue hearing your invites';
+       var message = this.t('ERROR_MESSAGE');
        this.emit(':ask', message);
     }
 });
-//Functionality to take action by Alexa according to the response of the person. 
-var endHandlers = Alexa.CreateStateHandler(states.ENDMODE, {
+//The function to take action by Alexa according to the response of the person. 
+var actionHandlers = Alexa.CreateStateHandler(states.ENDMODE, {
     'NewSession': function() {
         this.handler.state = '';
         this.emitWithState('NewSession');
     },
+    //If the person said yest to the invite, functionalities to add: updating the status of the invite and sending out the response
     'AMAZON.YesIntent': function() {
         this.attributes.countResponse = 2;
         this.handler.state = states.PARTYMODE;
         this.emitWithState('GetPartyIntent');
     },
+    //If the person said no to the invite, functionalities to add: updating the status of the invite and sending out the response
     'AMAZON.NoIntent': function() {
         this.attributes.countResponse = 1;
         this.handler.state = states.PARTYMODE;
         this.emitWithState('GetPartyIntent');
-        //console.log("NOINTENT")
     },
     'AMAZON.StopIntent': function () {
-        this.emit(':tell', "In a while crocodile");
+        this.emit(':tell', 'CANCEL_MESSAGE');
     },
     'AMAZON.CancelIntent': function () {
-        this.emit(':tell', "In a while crocodile");
+        this.emit(':tell', 'CANCEL_MESSAGE');
     },
     'SessionEndedRequest': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     }, 
 });
 
-//some messages gathered together
+//Messages used trhoughout the skill
 const languageStrings = {
     'en-US' : {
         translation: {
+            ANSWER_NO: "Sure? You should go out more often. Your social life is worse than mine. But okay, this party wasn\'t that cool anyways.",
+            ANSWER_REPROMPT: "Yes or No?",
+            ANSWER_YES: "Cool, I\'ve added it to your calendar.",
+            ASK_FOR_RSVP: "Do you want to go to",
+            CANCEL_MESSAGE: "In a while crocodile",
+            ERROR_MESSAGE: "Oops, something went wrong, say give me my partylist to continue hearing your invites",
+            HELP_MESSAGE: "If you want to hear your list of invites say give me my party list",
+            HELP_REPROMPT: "You can get your invites by saying give me my party list or you can exit by saying stop",
+            NO_PARTIES_IN_LIST: "Sorry matey, no more parties for you!",
+            STOP_MESSAGE: "Until next time; Party on!",
             WELCOME_MESSAGE: "I Like To Party! Do you like to party? Let's take a look at your invites",
             WELCOME_REPROMT: "Hello. Are you there? Would you like to hear your party list?",
-            //PARTIES: "Parteeeey", // not sure how this works, but we need this to get the party list
-            NO_PARTIES_IN_LIST: "Sorry matey, no more parties for you!",
-            HELP_MESSAGE: "Help",
-            HELP_REPROMPT: "Help reprompt",
-            STOP_MESSAGE: "Until next time; Party on!",
+            WHAT_NEXT: "What do you want to do next?",
         },
     },
 };
@@ -137,6 +137,6 @@ const languageStrings = {
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
     alexa.resources = languageStrings;
-    alexa.registerHandlers(startHandlers, partyHandlers, endHandlers);
+    alexa.registerHandlers(startHandlers, partyHandlers, actionHandlers);
     alexa.execute();
 };
